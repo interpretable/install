5dac4401621c6d978b49077bd53eb7e4d6e89f52
Yassin Siouda <yassin@siouda.com>
2019-05-13 15:55:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
+# Install a LAMP Server
 apt-get update
 sudo apt install apache2
 sudo ufw allow “Apache Full”
-sudo apt install php libapache2-mod-php php-mbstring php-xmlrpc php-soap php-mysql php-gd php-xml php-cli php-zip php-bcmath php-tokenizer php-json php-pear
-sudo apt install mysql-server
-sudo apt install git curl
+sudo apt -y install php libapache2-mod-php php-mbstring php-xmlrpc php-soap php-mysql php-gd php-xml php-cli php-zip php-bcmath php-tokenizer php-json php-pear
+sudo apt -y install mysql-server
+sudo apt -y install git curl
 sudo mysql_secure_installation
-#use erasme as root password
+
+# Install Composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 sudo chmod +x /usr/local/bin/composer
 mkdir /var/www/html
 cd /var/www/html
 sudo rm index.html
+
 sudo git clone https://github.com/interpretable/install ./
+
+# Enable mod_rewrite
+sudo a2enmod rewrite
+sudo service apache2 restart
+
 
 #copy apache configuration file
 sudo a2ensite interpretable.conf
@@ -24,7 +32,7 @@ sudo a2ensite interpretable.conf
 sudo sh ./start_dev.sh
 
 #remoteit tools
-sudo apt install openssh-server
+sudo apt -y install openssh-server
 sudo systemctl status ssh
 curl -LkO https://raw.githubusercontent.com/remoteit/installer/master/scripts/auto-install.sh
 chmod +x ./auto-install.sh
