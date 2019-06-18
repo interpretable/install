10595b5dd4587af833a11d17217b1ead92bfdf58
ysiouda <ysiouda@erasme.org>
2019-06-18 09:26:53 +0200

    enable cron script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo sh scripts/lamp.sh
 sudo sh scripts/laravel.sh
-#sudo sh scripts/cron.sh
+sudo sh scripts/cron.sh
 
 # Default BO url
 echo 'Front access : http://localhost/front/public/scenarios'
