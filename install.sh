b8794fe276d3e22766c387455dc7e40f369ae66a
ysiouda <ysiouda@erasme.org>
2019-06-24 12:07:49 +0200

    Disabling cron tasks

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 sudo sh scripts/lamp.sh
 sudo sh scripts/laravel.sh
-sudo sh scripts/cron.sh
+sudo sh scripts/autostart.sh
+#sudo sh scripts/cron.sh
 
 # Default BO url
 echo 'Front access : http://localhost/front/public/scenarios'
